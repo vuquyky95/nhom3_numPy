767542ec5ef6f397a2cc5c861d38a536437f29bb
Hoang4nh <145426012+Hoang4nh@users.noreply.github.com>
2023-09-19 14:41:13 +0700

    Update giai_he_PT.py

--- giai_he_PT.py
+++ giai_he_PT.py
@@ -2,13 +2,13 @@
 # Yêu cầu hoàn chỉnh lại đoạn code
 #để có 1 app giải hệ phương trình có n phương trình n ẩn
 import numpy as np
-A = np.array([(1,2),(3,4)])
-B = np.array([5,6])
-A1  = np.linalg.inv(A) # tạo ma trận nghich đảo
+A = np.array([(2,2),(3,5)])
+B = np.array([5,8])
+C  = np.linalg.inv(A) # tạo ma trận nghich đảo
 print(A)
 print(B)
-print(A1)
-X = np.dot(A1,B)
+print(C)
+X = np.dot(C,B)
 print('Nghiem cua he:',X)
-print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-print('bbbbbbbbbbbbbbbbbbbbbbb')
+print('Hello nhom 3')
+print('abcxyz')
